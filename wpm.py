f40fa17e9f41485db6697d37c3af8b7efffe1c49
Yezdani01 <yezdanihamza@gmail.com>
2025-09-20 17:42:27 +0530

    few changes

--- wpm.py
+++ wpm.py
@@ -1,13 +1,55 @@
 import curses
 from curses import wrapper
 
+
+def start_screen(stdscr):
+    stdscr.clear()
+    stdscr.addstr("Welcome to the WPM-Typing Test! ")
+    stdscr.addstr("\n Press any key to begin")
+    stdscr.refresh()
+    stdscr.getkey()
+
+
+def display_text(stdscr,target,current,wpm = 0):
+    stdscr.addstr(target_text)
+
+    for i, char in enumerate(current_text):
+        stdscr.addstr(char, curses.color_pair(1))
+    
+
+
+
+
+
+def wpm_test(stdscr):
+    target_text = "Hello world this is some test text for this application"
+    current_text = []
+
+
+
+    while True:
+        stdscr.clear()
+
+        stdscr.refresh()
+
+        key  = stdscr.getkey()
+
+        if ord(key) == 27:
+            break
+        if key in ("KEY_BACKSPACE", '\b', "\x7f"):
+            if len(current_text) > 0:
+                current_text.pop()
+        else:
+            current_text.append(key)
+
+        # current_text.append(key)
+
+
 def main(stdscr):
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
     curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
-    stdscr.clear()
-    stdscr.addstr("Hello world!",curses.color_pair(2))
-    stdscr.refresh()
-    stdscr.getkey()
+    start_screen(stdscr)
+    wpm_test(stdscr)
 
 wrapper(main)
